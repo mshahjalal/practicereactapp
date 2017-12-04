6337421765801384d503aa7523e841d56bbabe83
Md. Shahjalal <shahjalaldk@gmail.com>
2017-12-05 00:26:36 +0600

    add client side router

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import logo from '../logo.svg';
+import '../App.css';
+import Header from '../layouts/header.js'
+import Main from '../layouts/main.js'
+
+const App = () => (
+  <div>
+    <Header />
+    <Main />
+  </div>
+);
+
+export default App;
