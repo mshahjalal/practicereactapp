4c337eade7e55408fd6770ec04380abc933eef1a
Md. Shahjalal <shahjalaldk@gmail.com>
2017-12-05 11:08:12 +0600

    add language

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { translate, Trans } from 'react-i18next';
 //import logo from '../logo.svg';
 import '../App.css';
 import Header from '../layouts/header.js'
 import Main from '../layouts/main.js'
 
-const App = () => (
+/*const App = () => (
   <div>
     <Header />
     <Main />
   </div>
-);
+);*/
 
-export default App;
+class App extends Component {
+	render() {
+
+		const { t, i18n } = this.props;
+
+	    const changeLanguage = (lng) => {
+	      i18n.changeLanguage(lng);
+	    }
+
+		return (
+			<div>
+				<button onClick={() => changeLanguage('bd')}>Bangla</button>
+          		<button onClick={() => changeLanguage('en')}>English</button>
+    			<Header />
+    			<h2>{t('home')}</h2>
+    			<div>{t('labels.name')}</div>
+    			<Main />
+  			</div>
+		);
+	}
+}
+
+export default translate('translations')(App);
+//export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import './i18n.js';
 
 ReactDOM.render(
 	<BrowserRouter>
