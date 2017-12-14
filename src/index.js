62249f9ea5e04960f5ae7fed44dbb13d8798a018
Md. Shahjalal <shahjalaldk@gmail.com>
2017-12-14 10:54:02 +0600

    webpack

--- src/index.js
+++ src/index.js
@@ -1,16 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
-//import './app.scss';
-import App from './components/App';
+import './index.css';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import './i18n.js';
 
-//import 'bootstrap/dist/css/bootstrap.css';
-
-
-ReactDOM.render(
-	<BrowserRouter>
-    <App />
-  </BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
