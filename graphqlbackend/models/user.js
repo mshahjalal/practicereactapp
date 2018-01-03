1798b6536b6bcee826b8aba0c3bfd88ca98a78a8
Md. Shahjalal <shahjalaldk@gmail.com>
2018-01-04 00:37:55 +0600

    separate model

---
+++ graphqlbackend/models/user.js
@@ -0,0 +1,21 @@
+export default (sequelize, DataTypes) => {
+	const User = sequelize.define(
+	    'user',
+	    {
+	    	username: {
+	    		type: DataTypes.STRING,
+        		unique: true
+	    	},
+	    	email: {
+	    		type: DataTypes.STRING,
+        		unique: true
+	    	}
+	    }
+    );
+
+    User.associate = (models) => {
+
+    };
+
+    return User;
+};
