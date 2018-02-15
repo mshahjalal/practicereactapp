2ad21a6c29657f960e7995a01eab8054b71817db
Md. Shahjalal <shahjalaldk@gmail.com>
2018-02-15 23:48:14 +0600

    add login

---
+++ posclient/src/routes/Login.js
@@ -0,0 +1,69 @@
+import React from 'react';
+import { extendObservable } from 'mobx';
+import { observer } from 'mobx-react';
+import { gql, graphql } from 'react-apollo';
+
+class Login extends React.Component {
+  constructor(props) {
+    super(props);
+
+    extendObservable(this, {
+      email: '',
+      password: '',
+    });
+  }
+
+  onSubmit = async () => {
+    const { email, password } = this;
+    const response = await this.props.mutate({
+      variables: { email, password },
+    });
+    console.log(response);
+    const { ok, token, refreshToken } = response.data.login;
+    if (ok) {
+      localStorage.setItem('token', token);
+      localStorage.setItem('refreshToken', refreshToken);
+    }
+  };
+
+  onChange = e => {
+    const { name, value } = e.target;
+    this[name] = value;
+  };
+
+  render() {
+    const { email, password } = this;
+
+    return (
+      <div text>
+        <h2>Login</h2>
+        <input name="email" onChange={this.onChange} value={email} placeholder="Email" fluid />
+        <input
+          name="password"
+          onChange={this.onChange}
+          value={password}
+          type="password"
+          placeholder="Password"
+          fluid
+        />
+        <button onClick={this.onSubmit}>Submit</button>
+      </div>
+    );
+  }
+}
+
+const loginMutation = gql`
+  mutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      ok
+      token
+      refreshToken
+      errors {
+        path
+        message
+      }
+    }
+  }
+`;
+
+export default graphql(loginMutation)(observer(Login));
