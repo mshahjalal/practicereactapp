7ade0982f22032776a014098db4b301a0a583908
Md. Shahjalal <shahjalaldk@gmail.com>
2018-02-17 00:52:30 +0600

    server side login validation

--- posserver/resolvers/user.js
+++ posserver/resolvers/user.js
@@ -1,9 +1,11 @@
 import bcrypt from 'bcrypt';
 import _ from 'lodash';
+
 import { tryLogin } from '../auth';
 
 const formatErrors = (e, models) => {
   if (e instanceof models.sequelize.ValidationError) {
+    //  _.pick({a: 1, b: 2}, 'a') => {a: 1}
     return e.errors.map(x => _.pick(x, ['path', 'message']));
   }
   return [{ path: 'name', message: 'something went wrong' }];
@@ -16,23 +18,10 @@ export default {
   },
   Mutation: {
     login: (parent, { email, password }, { models, SECRET, SECRET2 }) =>
-      tryLogin(email, password, models, SECRET),
-    register: async (parent, { password, ...otherArgs }, { models }) => {
+      tryLogin(email, password, models, SECRET, SECRET2),
+    register: async (parent, args, { models }) => {
       try {
-        if (password.length < 5 || password.length > 100) {
-          return {
-            ok: false,
-            errors: [
-              {
-                path: 'password',
-                message: 'The password needs to be between 5 and 100 characters long',
-              },
-            ],
-          };
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 12);
-        const user = await models.User.create({ ...otherArgs, password: hashedPassword });
+        const user = await models.User.create(args);
 
         return {
           ok: true,
@@ -44,6 +33,6 @@ export default {
           errors: formatErrors(err, models),
         };
       }
-    }
-  }
+    },
+  },
 };
